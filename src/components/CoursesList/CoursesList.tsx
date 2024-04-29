b2168319e6b8c8936b9b9b57f73e4ab0fefbe2ca
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-04-29 13:27:26 +0400

    feat: comment out the code that makes a get request to get courses

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -5,9 +5,9 @@ import CoursesListByCategory from '../CoursesListByCategory/CoursesListByCategor
 import styles from './CoursesList.module.css';
 
 const CoursesList = () => {
-  const [coursesData, setCoursesData] = useState<ICourse[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  // const [coursesData, setCoursesData] = useState<ICourse[]>([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState('');
 
   // Grouping data by categories
   const groupedData = useMemo<Record<string, ICourse[]>>(() => {
@@ -18,25 +18,25 @@ const CoursesList = () => {
     }, {});
   }, []);
 
-  useEffect(() => {
-    setIsLoading(true);
-    const serverUrl = process.env.REACT_APP_SERVER_URL;
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   const serverUrl = process.env.REACT_APP_SERVER_URL;
 
-    // Fetch data
-    fetch(`${serverUrl}/course`)
-      .then((response) => response.json())
-      .then((data) => {
-        setCoursesData(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error?.message);
-        setIsLoading(false);
-      });
-  }, []);
+  //   // Fetch data
+  //   fetch(`${serverUrl}/course`)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setCoursesData(data);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       setError(error?.message);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  // if (isLoading) return <p>Loading...</p>;
+  // if (error) return <p>Error: {error}</p>;
 
   return (
     <div className={styles.container}>
