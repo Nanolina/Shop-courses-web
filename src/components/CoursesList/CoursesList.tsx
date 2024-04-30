fe6d49f876fe8bb4109daedd3a0e500f33fd9fa1
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-04-30 09:50:41 +0400

    feat: add real data and fetch request to backend for getting all courses

--- src/components/CoursesList/CoursesList.tsx
+++ src/components/CoursesList/CoursesList.tsx
@@ -1,42 +1,43 @@
+import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
-import { data } from '../../data.ts';
 import { ICourse } from '../../types.ts';
 import CoursesListByCategory from '../CoursesListByCategory/CoursesListByCategory.tsx';
 import styles from './CoursesList.module.css';
 
 const CoursesList = () => {
-  // const [coursesData, setCoursesData] = useState<ICourse[]>([]);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState('');
+  const [coursesData, setCoursesData] = useState<ICourse[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
 
   // Grouping data by categories
   const groupedData = useMemo<Record<string, ICourse[]>>(() => {
-    return data.reduce<Record<string, ICourse[]>>((acc, course) => {
+    return coursesData.reduce<Record<string, ICourse[]>>((acc, course) => {
       acc[course.category] = acc[course.category] || [];
       acc[course.category].push(course);
       return acc;
     }, {});
+  }, [coursesData]);
+
+  async function getCourseData() {
+    try {
+      const serverUrl = process.env.REACT_APP_SERVER_URL;
+      const response = await axios.get(`${serverUrl}/course`);
+      setCoursesData(response.data);
+      setIsLoading(false);
+      return response.data;
+    } catch (error: any) {
+      setError(error?.message || error);
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    getCourseData();
   }, []);
 
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   const serverUrl = process.env.REACT_APP_SERVER_URL;
-
-  //   // Fetch data
-  //   fetch(`${serverUrl}/course`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setCoursesData(data);
-  //       setIsLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       setError(error?.message);
-  //       setIsLoading(false);
-  //     });
-  // }, []);
-
-  // if (isLoading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error}</p>;
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div className={styles.container}>
