30da032628f89c020e804479b6b670ef62eca668
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-01 20:45:56 +0400

    revert: remove image from create form
    Due to the fact that sending userId is possible from the frontend, but it is insecure,
    it was decided to remove the image from the form and upload it only through the bot.
    Thus, any change of courses will be done through the bot,
    which will take the userId on its own, instead of getting it from the request

--- src/pages/CreateFormPage/CreateFormPage.tsx
+++ src/pages/CreateFormPage/CreateFormPage.tsx
@@ -1,51 +1,20 @@
-import axios from 'axios';
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { animateScroll as scroll } from 'react-scroll';
 import { IOption } from '../../types';
 import Container from '../../ui/Container/Container';
-import ImagePreview from '../../ui/ImagePreview/ImagePreview';
-import ImageUpload from '../../ui/ImageUpload/ImageUpload';
 import Label from '../../ui/Label/Label';
-import { Loader } from '../../ui/Loader/Loader';
 import Select from '../../ui/Select/Select';
 import TextInput from '../../ui/TextInput/TextInput';
 import Textarea from '../../ui/Textarea/Textarea';
 
 const tg = window.Telegram.WebApp;
-const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 function CreateFormPage() {
-  const navigate = useNavigate();
-
   const [name, setName] = useState<string>('');
   const [description, setDescription] = useState<string>('');
   const [category, setCategory] = useState<string>('');
   const [subcategory, setSubcategory] = useState<string>('');
   const [price, setPrice] = useState<number>(0);
   const [currency, setCurrency] = useState<string>('');
-  const [imageFile, setImageFile] = useState<File | null>(null);
-  const [imagePreview, setImagePreview] = useState<string>('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const setImage = (file: File | null) => {
-    setImageFile(file);
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setImagePreview(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    } else {
-      setImagePreview('');
-    }
-  };
-
-  const handleRemoveImage = () => {
-    setImagePreview('');
-    setImageFile(null);
-  };
 
   const categoryOptions: IOption[] = [
     { value: 'technology', label: 'Technology' },
@@ -77,54 +46,17 @@ function CreateFormPage() {
     { value: 'TON', label: 'The Open Network (TON)' },
   ];
 
-  const onSendData = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const userId = tg.initDataUnsafe.user?.id;
-      const userName = tg.initDataUnsafe.user?.username;
-      const formData = new FormData();
-
-      // Required fields
-      formData.append('name', name);
-      formData.append('category', category);
-      formData.append('price', price.toString());
-      formData.append('currency', currency);
-
-      // Optional fields
-      if (userId) formData.append('userId', userId?.toString());
-      if (userName) formData.append('userName', userName);
-      if (subcategory) formData.append('subcategory', subcategory);
-      if (description) formData.append('description', description);
-      if (imageFile) formData.append('image', imageFile);
-
-      // Request
-      const courseApiUrl = `${serverUrl}/course`;
-      const response = await axios.post(courseApiUrl, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      // Response
-      if (response.status === 201) {
-        const courseId = response.data.id;
-        navigate(`/course/${courseId}`);
-      }
-      setIsLoading(false);
-    } catch (error: any) {
-      setError(error?.message || error);
-      setIsLoading(false);
-    }
-  }, [
-    name,
-    category,
-    price,
-    currency,
-    subcategory,
-    description,
-    imageFile,
-    navigate,
-  ]);
+  const onSendData = useCallback(() => {
+    const course = {
+      name,
+      description,
+      category,
+      subcategory,
+      price,
+      currency,
+    };
+    tg.sendData(JSON.stringify(course));
+  }, [category, currency, description, name, price, subcategory]);
 
   useEffect(() => {
     tg.MainButton.setParams({
@@ -146,13 +78,6 @@ function CreateFormPage() {
       tg.offEvent('mainButtonClicked', onSendData);
     };
   }, [onSendData]);
-
-  useEffect(() => {
-    if (imagePreview) scroll.scrollToBottom();
-  }, [imagePreview]);
-
-  if (isLoading) return <Loader />;
-  if (error) return <p>Error: {error}</p>;
 
   return (
     <Container>
@@ -208,19 +133,6 @@ function CreateFormPage() {
         }
         options={currencyOptions}
       />
-      <Label
-        text="The main image for your course"
-        style={{
-          isCenter: true,
-        }}
-      />
-      <ImageUpload onImageChange={setImage} />
-      {imagePreview && (
-        <ImagePreview
-          imagePreview={imagePreview}
-          removeImage={handleRemoveImage}
-        />
-      )}
     </Container>
   );
 }
