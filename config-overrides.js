8d038d148c888e076e58a26a553ef87547b1e683
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-05-15 14:06:11 +0400

    feat: remove unnecessary modules

--- config-overrides.js
+++ config-overrides.js
@@ -4,15 +4,6 @@ module.exports = function override(config) {
   // Add polyfills for Node.js core modules
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    buffer: require.resolve('buffer'),
-    stream: require.resolve('stream-browserify'),
-    crypto: require.resolve('crypto-browserify'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify/browser'),
-    url: require.resolve('url'),
-    assert: require.resolve('assert'),
-    process: require.resolve('process/browser.js'),
   };
 
   // Add plugins to provide Node.js core modules
