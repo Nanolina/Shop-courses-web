de1c2b56c663d67ca19ac4862569b3d70f63d0af
Alina Perevozchikova <alinka.12.12.98@gmail.com>
2024-06-25 00:38:48 +0400

    feat: add points to Main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import 'swiper/css';
 import CoursesList from '../../components/CoursesList/CoursesList';
 import Container from '../../ui/Container/Container';
+import Points from '../../ui/Points/Points';
 import SearchBar from '../../ui/SearchBar/SearchBar';
 
 const tg = window.Telegram.WebApp;
@@ -16,6 +17,7 @@ const MainPage: React.FC = () => {
 
   return (
     <Container>
+      <Points />
       <SearchBar />
       <CoursesList />
     </Container>
