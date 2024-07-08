96113c869435e7191cec8348ef3ba98bddc206b9
Sneganna Tumanova <Sneganna.snow@mail.ru>
2024-07-08 17:36:26 +0300

    feat: add role validation on the Modules, Lessons

--- src/components/CoursePartList/CoursePartList.tsx
+++ src/components/CoursePartList/CoursePartList.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FiBox } from 'react-icons/fi';
 import { getTranslatedType } from '../../functions';
 import ReadyCoursePart from '../ReadyCoursePart/ReadyCoursePart';
 import { ICoursePartListProps } from '../types';
 import styles from './CoursePartList.module.css';
+import { SELLER } from '../../consts';
 
 function CoursePartList({
   type,
@@ -14,8 +15,15 @@ function CoursePartList({
   updateItems,
 }: ICoursePartListProps) {
   const { t } = useTranslation();
+  const [isSeller, setIsSeller] = useState<boolean>(false);
 
   const { singular_genitive_case } = getTranslatedType(type, t);
+
+  useEffect(() => {
+    if (role === SELLER) {
+      setIsSeller(true);
+    }
+  }, [role]);
 
   return (
     <>
@@ -24,7 +32,13 @@ function CoursePartList({
           <img src="/no-items.png" alt="No items" className={styles.image} />
           <div className={styles.notCreatedContainer}>
             <FiBox size={24} color="var(--tg-theme-accent-text-color)" />
-            <h4>{t('not_created_type', { type: singular_genitive_case })}</h4>
+            {isSeller ? (
+              <h4>{t('not_created_type', { type: singular_genitive_case })}</h4>
+            ) : (
+              <h4>
+                {t('no_type_available', { type: singular_genitive_case })}
+              </h4>
+            )}
           </div>
         </>
       ) : (
